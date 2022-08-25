0a00801e3773e3070217ccd57a76bec3526f263f
Thai Truong <thai.truong@coles.com.au>
2022-08-25 04:04:27 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,1 +1,5 @@
-resource_group_name = azurerm_resource_group.myresourcegroup.name
+module "network" {
+  source  = "app.terraform.io/Coles-TT/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
